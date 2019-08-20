7530d35a19f3492b035745c40f9cdb743a36915b
niedermansam <niederman.sam@gmail.com>
2019-08-19 22:09:26 -0600

    update projects page

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ const Home = () => (
     <h1 id="greeting">Hi! My name is Sam.</h1>
     <div id="about-me">
       <p> I'm a web developer and data analyst. </p>
-      <p> I recently graduated from the University of Montana with a Master's degree in Economics. </p>
-      <p> I have experience with HTML, CSS, and Javascript, along with React, and node.js. </p>
-      <p> I use R/Shiny and Stata for more complex data visualitzation, analysis, and econometric modeling.</p>
+      <div className="about-bullet"> <i className="fa fa-graduation-cap fa-3x"></i> <p> I recently graduated from the University of Montana with a Master's degree in Economics. </p></div>
+      <div className="about-bullet"><i className="fa fa-code fa-3x"></i> <p> I have experience with HTML, CSS, and Javascript, along with React, and node.js. </p></div>
+    <div className="about-bullet"> <i className="fa fa-bar-chart fa-3x"></i> <p> I use R/Shiny and Stata for more complex data visualitzation, analysis, and econometric modeling.</p></div>
     </div>
   </div>
 )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ const projectArr = [
     the NOAA API to get a forecast for that point.`,
     url: "www.averageanalytics.com/snow-forecast",
     github: 'https://github.com/niedermansam/snow-forecast',
-    keywords: ["API", "Javascript", "leaflet.js"],
+    keywords: [ "Javascript", "APIs", "Data Collection", "leaflet.js", "GIS"],
     img: require('./photos/ski-resort-explorer.PNG')
   },
   {name: "Census Time Series", description: `A Javascript application
@@ -15,32 +15,35 @@ const projectArr = [
      2010 to present.`,
     url: 'www.averageanalytics.com/census-search/',
     github: 'https://github.com/niedermansam/census-search',
-    keywords: ["API", "data visualization", ""],
+    keywords: ["APIs", "Plot.ly", "Data Visualization", "Data Collection", "JQuery"],
     img: require('./photos/census-searcher-tool.PNG')
   },
   {name: "Trump Twittermap", description: `An R/Shiny application
     that visualizes the twitter habits of the 45th President of the
-    United States. (This one takes a while to load and is out of date)`,
+    United States.`,
     url: 'http://68.183.17.153:3838/trump-tweets/',
     github: '',
-    keywords: ["R/Shiny", "data visualization", "ggplot"],
+    keywords: ["R/Shiny", "Data Visualization", "ggplot"],
     img: require('./photos/trump-twitter-map.PNG')
   }
 ]
 
 function Projects() {
   return (
-    <div className="text-container">
+    <div className="text-container project-page">
       <h1>Projects</h1>
-      {
-        projectArr.map( (project, index) => {
-          project.key = index;
-          return new ProjectCard(project);
-        })
-      }
+      <div className="projects">
+        {
+          projectArr.map( (project, index) => {
+            project.key = index;
+            return new ProjectCard(project);
+          })
+        }
+      </div>
     </div>
   );
 }
+
 
 function ProjectCard(props){
 
@@ -48,16 +51,31 @@ function ProjectCard(props){
 
 
   return (
-    <div className="project-card" style={divStyle} key={props.key}>
+    <div className="project-card" key={props.key}>
+      <a href={"//" + props.url} style={{textDecoration: 'none'}} target="_blank" rel="noopener noreferrer">
+    <div className="project-card-header" style={divStyle}>
         <h2 className="project-name">{props.name}</h2>
+
         <div className="project-card-overlay">
         <p className="project-description">{props.description}</p>
         </div>
-        <div className="project-button-bar">
-        <a href={props.url} target="_blank" rel="noopener noreferrer"><button>Project</button></a>
-        <a href={ props.github} target="_blank" rel="noopener noreferrer"><button>GitHub</button></a>
-        </div>
+
+
     </div>
+    </a>
+
+    <div className="project-button-bar">
+    <a href={props.url} target="_blank" rel="noopener noreferrer"><button>Project</button></a>
+    <a href={ props.github} target="_blank" rel="noopener noreferrer"><button>GitHub</button></a>
+    </div>
+
+    <div className="project-skills">
+      <h3>Skills Used:</h3>
+      <ul>
+        {props.keywords.map((keyword, index) => <li key={index}>{keyword}</li>)}
+      </ul>
+    </div>
+  </div>
   )
 }
 
